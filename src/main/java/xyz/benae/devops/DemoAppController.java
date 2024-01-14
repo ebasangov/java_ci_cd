d613fcf6b93a3db94c607d73a69382c72d7a2853
erb <erb@benae.xyz>
2024-01-13 21:15:50 -0500

    Removed this unused RestController

--- src/main/java/xyz/benae/devops/DemoAppController.java
+++ src/main/java/xyz/benae/devops/DemoAppController.java
@@ -2,7 +2,7 @@ package xyz.benae.devops;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @Controller
 public class DemoAppController {
